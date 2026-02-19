cbfec6b354446331a3fcaa85b3b1a0d42d9e69b8
ekrzys <e.r.krzys@gmail.com>
2026-02-18 21:37:45 -0500

    Update feed.js

--- api/feed.js
+++ api/feed.js
@@ -1,7 +1,7 @@
 export  default async function handler(req, res) {
   res.status(200).json({ 
     reqUrl: req.url,
-    reqQuery: req.query,
+    reqQuery:  req.query,
     reqHeaders: req.headers
   });
 }
